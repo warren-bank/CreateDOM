a3919a0af8e8c78b1c1f9460051460d20edb7574
Warren R Bank <warren.b.415@gmail.com>
2014-07-16 09:56:27 -0700

    updates
    * declaration syntax from function x(){} to var x=function(){}
    * added optional "doc" parameter, which defaults to "document"
    * re-enabled support for arbitrary tag attributes;
      this had been commented out
    * always returns the DOM element(s) created;
      original version did not do so when an "id" was used to append them into the DOM

--- $C.js
+++ $C.js
@@ -1,22 +1,36 @@
 /*
+ * --------------------------------------------------------
+ * original script
+ *     name:    CreateDOM - $C()
+ *     summary: Create DOM Stuctures Easily
+ *     url:     http://www.openjs.com/scripts/createdom/
  * author
  *     name:    Binny V Abraham
  *     email:   moc.liamg@avynnib
  *     url:     http://binnyva.com/me/
- * script
- *     name:    CreateDOM - $C()
- *     summary: Create DOM Stuctures Easily
- *     url:     http://www.openjs.com/scripts/createdom/
  * license
  *     name:    BSD License
  *     url:     http://www.openjs.com/license.php
+ * --------------------------------------------------------
+ * modified script
+ *     notes:   forked from original script on 07/16/2014
+ * author
+ *     name:    Warren R Bank
+ * license
+ *     name:    GPLv2
+ *     url:     http://www.gnu.org/licenses/gpl-2.0.txt
+ *     notes:   applies to diff between modified script and original.
+ *              This is more restrictive than BSD, and requires that
+ *              any and all derivative works also maintain a GPL (copy-left) license.
+ * --------------------------------------------------------
  */
 
-function $C(dom,id) {
+var $C = function(dom,id,doc) {
 	if(!id) var id = "";//The ID argument is optional
+	if(!doc) var doc = document;
 	//Most *necessary* HTML tags - make sure not to include any tags that is also a valid attribute name - eg 'cite'
-	var valid_tags = ",p,div,span,strong,em,u,img,pre,code,br,hr,a,script,link,table,tr,td,h1,h2,h3,h4,h5,h6,sup,sub,"+
-		"ul,ol,li,dd,dl,dt,form,input,textarea,legend,label,fieldset,select,option,blockquote,";//Begining and ending commas are intentional - don't remove them
+	//Begining and ending commas are intentional - don't remove them
+	var valid_tags = ",p,div,span,strong,em,u,img,pre,code,br,hr,a,script,link,table,tr,td,h1,h2,h3,h4,h5,h6,sup,sub,ul,ol,li,dd,dl,dt,form,input,textarea,legend,label,fieldset,select,option,blockquote,";
 	var html = new Array();
 	var non_alapha = new RegExp(/_\d*$/);
 	for(var tag in dom) {
@@ -31,11 +45,11 @@ function $C(dom,id) {
 			}
 			tag = tagname;
 		}
-		
+
 		tag = tag.replace(non_alapha,"");//Remove the numbers at the end
-		var ele = document.createElement(tag);
+		var ele = doc.createElement(tag);
 		//If the given attribute is a string, it is a text node
-		if(typeof(attributes) == "string") child = document.createTextNode(attributes);
+		if(typeof(attributes) == "string") child = doc.createTextNode(attributes);
 		else if(attributes) {//If it an array...
 			for(var att in attributes) {
 				var value = "";
@@ -55,8 +69,8 @@ function $C(dom,id) {
 					node[att] = value;
 					ele.appendChild($C(node,""));// :RECURSION:
 				}
-				else if(att == "text") child = document.createTextNode(value);//The text in the tag
-				//else ele.setAttribute(att,value);
+				else if(att == "text") child = doc.createTextNode(value);//The text in the tag
+				else ele.setAttribute(att,value);
 			}
 		}
 
@@ -64,12 +78,13 @@ function $C(dom,id) {
 		html.push(ele);
 	}
 
-	if(!id) {//If no node is given, return the created node.(Exits the function)
-		if(html.length == 1) return html[0];
-		return html;
-	}
 	//If a node/id was given, append the created elements to that element.
-	var node = id;
-	if(typeof id == "string") node = document.getElementById(id);//If the given argument is an id.
-	for(var i=0;el=html[i],i<html.length;i++) node.appendChild(el);
-}
+	if (id){
+		var node = id;
+		if(typeof id == "string") node = doc.getElementById(id);//If the given argument is an id.
+		for(var i=0;el=html[i],i<html.length;i++) node.appendChild(el);
+	}
+
+	if(html.length == 1) return html[0];
+	return html;
+};
