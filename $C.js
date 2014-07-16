2faca5cfedd37ef2b8afc0167cb9f58323fd5260
Warren R Bank <warren.b.415@gmail.com>
2014-07-16 10:37:00 -0700

    updates
    support for new tags:
      * "b"
      * "text"
    
    support for new tag attributes:
      * "condition"
    
    notes:
    ======
      * "condition" attribute:
          - optional
          - when present, should always be the first attribute in the object
          - boolean value that determines whether the entire tag should be ignored
    
      * "text" (tag/attribute):
          - the value may either be:
              * string
              * object: {"condition":boolean,"text":string}
    
      * when the DOM tree being created contains conditional elements,
        rather than:
            creating a base data structure,
            and then doing if/then statements to conditionally inject additional attributes,
            and then dealing with the fact that attribute order matters..
            and how to inject keys into a hash table so they iterate in the proper order..
            and then passing the resulting data structure to $C()
        the new "condition" attribute enables the creation of a single intuitive data structure,
        which contains all possible nodes,
        as well as an optional boolean value to indicate whether or not to include a node/branch.

--- $C.js
+++ $C.js
@@ -26,11 +26,10 @@
  */
 
 var $C = function(dom,id,doc) {
-	if(!id) var id = "";//The ID argument is optional
 	if(!doc) var doc = document;
 	//Most *necessary* HTML tags - make sure not to include any tags that is also a valid attribute name - eg 'cite'
 	//Begining and ending commas are intentional - don't remove them
-	var valid_tags = ",p,div,span,strong,em,u,img,pre,code,br,hr,a,script,link,table,tr,td,h1,h2,h3,h4,h5,h6,sup,sub,ul,ol,li,dd,dl,dt,form,input,textarea,legend,label,fieldset,select,option,blockquote,";
+	var valid_tags = ",b,p,div,span,strong,em,u,img,pre,code,br,hr,a,script,link,table,tr,td,h1,h2,h3,h4,h5,h6,sup,sub,ul,ol,li,dd,dl,dt,form,input,textarea,legend,label,fieldset,select,option,blockquote,";
 	var html = new Array();
 	var non_alapha = new RegExp(/_\d*$/);
 	for(var tag in dom) {
@@ -45,9 +44,27 @@ var $C = function(dom,id,doc) {
 			}
 			tag = tagname;
 		}
-
 		tag = tag.replace(non_alapha,"");//Remove the numbers at the end
-		var ele = doc.createElement(tag);
+
+		var ele;
+		if (tag == "text"){
+			if (typeof attributes === "string"){
+				ele = doc.createTextNode(attributes);
+			}
+			else if (
+					(typeof attributes === "object")
+				&&	(attributes !== null)
+				&&	(attributes.text)
+				&&	(attributes.condition || (typeof attributes.condition === "undefined"))
+			){
+				ele = doc.createTextNode(attributes.text);
+			}
+			attributes = false;
+		}
+		else {
+			ele = doc.createElement(tag);
+		}
+
 		//If the given attribute is a string, it is a text node
 		if(typeof(attributes) == "string") child = doc.createTextNode(attributes);
 		else if(attributes) {//If it an array...
@@ -63,19 +80,35 @@ var $C = function(dom,id,doc) {
 				}
 
 				att = att.replace(non_alapha,"");//Remove the numbers at the end - to solve the problem of non unique indexes
-				if(valid_tags.indexOf(","+att+",") != -1) { //If the attribute is a valid tag,
+				if(att == "condition"){if (! value) {ele = undefined; break;}}
+				else if(valid_tags.indexOf(","+att+",") != -1) { //If the attribute is a valid tag,
 					//Find the dom sturcture of that tag.
 					var node = new Object;
 					node[att] = value;
-					ele.appendChild($C(node,""));// :RECURSION:
+					ele.appendChild($C(node,"",doc));// :RECURSION:
 				}
-				else if(att == "text") child = doc.createTextNode(value);//The text in the tag
+				else if(att == "text") {
+					//The text in the tag
+					if (typeof value === "string"){
+						child = doc.createTextNode(value);
+					}
+					else if (
+							(typeof value === "object")
+						&&	(value !== null)
+						&&	(value.text)
+						&&	(value.condition || (typeof value.condition === "undefined"))
+					){
+						child = doc.createTextNode(value.text);
+					}
+				}
 				else ele.setAttribute(att,value);
 			}
 		}
 
-		if(child && attributes) ele.appendChild(child);//Append the child if it exists
-		html.push(ele);
+		if(ele){
+			if(child && attributes) ele.appendChild(child);//Append the child if it exists
+			html.push(ele);
+		}
 	}
 
 	//If a node/id was given, append the created elements to that element.
